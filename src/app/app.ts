171c96f81b2024b5b9daedb2c3491577a028ce04
Marwa CHEIKH <cheikhmarwa01@gmail.com>
2026-04-05 12:28:20 +0200

    "Sauvegarde de mon code avant le pull de l'équipe"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// 1. On indique à l'ordinateur où trouver vos DEUX composants
+import { GererDossier } from './gerer-dossier/gerer-dossier';
+import { AjouterRdv } from './ajouter-rdv/ajouter-rdv';
+import { DicterOrdonnance } from './dicter-ordonnance/dicter-ordonnance';
+import { VueOrdonnance } from './vue-ordonnance/vue-ordonnance';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // 2. On les ajoute officiellement tous les deux dans la liste
+  imports: [RouterOutlet, GererDossier, AjouterRdv, DicterOrdonnance, VueOrdonnance],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('front-gestion-dossier');
